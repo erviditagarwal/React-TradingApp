727b48a021fcddbeffca8784a1ddfc940c8fca74
Vidit Cyber <viditagarwal4400@gmail.com>
2023-09-10 14:00:13 +0530

    Navigation Page

--- Fta/App.js
+++ Fta/App.js
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Home from './src/screens/Home';
 
+
 export default function App() {
   return (
     
-     
+     <view>
         <Home/>
+       
     
+     </view>
+       
 
    
   );
--- Fta/src/screens/Home.js
+++ Fta/src/screens/Home.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Navigation from '../../navigation/Navigation'
 
 const Home = () => {
   return (
     <View>
       <Text>Welcome To Trading App Home</Text>
+      <Navigation/>
     </View>
   )
 }
